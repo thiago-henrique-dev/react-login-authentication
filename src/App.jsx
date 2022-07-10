7659e1daf2c70e3d6b3ac4e4847bb114660c1fe0
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-07-10 11:53:45 -0300

    created form processs..

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css';
 import LoginPage from './pages/loginPages';
-import HomePage from './pages/HomePage';
 
 
 function App() {
   return (
     <div className="App">
      <LoginPage/>
-     <HomePage/>
     </div>
   );
 }
